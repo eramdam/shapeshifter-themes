fc35e88435a12cc784d84343facc26f7d9e6860f
Damien Erambert <damien.erambert@me.com>
2018-03-25 21:57:16 -0700

    :sparkles: run prettier-eslint

--- server.js
+++ server.js
@@ -1,37 +1,40 @@
+require('dotenv').config();
+
 /* Setting things up. */
-var path = require('path'),
-    express = require('express'),
-    app = express(),   
-    Twit = require('twit'),
-    config = {
-    /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */      
-      twitter: {
-        consumer_key: process.env.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,
-        access_token: process.env.ACCESS_TOKEN,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET
-      }
-    },
-    T = new Twit(config.twitter);
+const express = require('express');
+
+const app = express();
+const Twit = require('twit');
+
+const config = {
+  /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */
+
+  twitter: {
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  },
+};
+const T = new Twit(config.twitter);
 
 app.use(express.static('public'));
 
 /* You can use uptimerobot.com or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
 
-app.all("/" + process.env.BOT_ENDPOINT, function (req, res) {
+app.all(`/${process.env.BOT_ENDPOINT}`, (req, res) => {
   /* The example below tweets out "Hello world!". */
-  T.post('statuses/update', { status: 'hello world 👋' }, function(err, data, response) {
-    if (err){
+  T.post('statuses/update', { status: 'hello world 👋' }, (err) => {
+    if (err) {
       res.sendStatus(500);
       console.log('Error!');
       console.log(err);
-    }
-    else{
+    } else {
       res.sendStatus(200);
     }
   });
 });
 
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your bot is running on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT, () => {
+  console.log(`Your bot is running on port ${listener.address().port}`);
 });
