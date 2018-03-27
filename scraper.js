6f59773ee9d32fa6ff4c51ceec360f556dd6cb9e
Damien Erambert <damien.erambert@me.com>
2018-03-26 20:18:06 -0700

    :wrench: export interfacelift's code into its own module

--- scraper.js
+++ scraper.js
@@ -1,66 +1,58 @@
 /* eslint no-await-in-loop: 0 */
 const fetch = require('node-fetch');
-const cheerio = require('cheerio');
 const fs = require('fs');
 const path = require('path');
+const { isString } = require('lodash');
+const Interfacelift = require('./scrapers/interfacelift');
 
 // http://macgui.com/downloads/?mode=category&cat_id=10
 // https://web.archive.org/web/20110126155426/https://interfacelift.com/themes-mac/
 
 const sleep = n => new Promise(resolve => setTimeout(resolve, n));
 
-const fetchInterfaceLiftPage = (n) => {
-  console.log(`Fetching page #${n}`);
-  return fetch(`https://web.archive.org/web/20101203061123/http://interfacelift.com:80/themes-mac/index.php?sort=date&id=&page=${n}`)
-    .then(res => res.text())
-    .then((body) => {
-      console.log(`Got page #${n}`);
-      const $ = cheerio.load(body);
-
-      return $('.list_preview')
-        .map((i, el) => {
-          const $preview = $(el);
-
-          return {
-            thumbnail: $preview.find('img').attr('src'),
-            name: $preview
-              .next('.list_text')
-              .find('h1')
-              .text(),
-            author: $preview
-              .next('.list_text')
-              .find('h4')
-              .text()
-              .replace('by ', ''),
-          };
-        })
-        .get();
-    });
-};
+const fetchPage = url => fetch(url).then(res => res.text());
 
 async function run() {
   const themesData = [];
+  const { BASE_URL, extractThemesFromBody, getNextPage } = Interfacelift;
+  let nextPage = BASE_URL;
 
-  for (let index = 1; index < 18; index += 1) {
-    await sleep(1000);
-    themesData.push(...(await fetchInterfaceLiftPage(index)));
-    console.log(themesData.length, 'themes');
+  while (isString(nextPage)) {
+    await sleep(200);
+    const pageBody = await fetchPage(`https://web.archive.org${nextPage}`);
+    themesData.push(...extractThemesFromBody(pageBody));
+    nextPage = getNextPage(pageBody);
+    console.log('Got themes', themesData.length);
+
+    if (isString(nextPage)) {
+      console.log('Next page', nextPage);
+    } else {
+      console.log('No next page, all done!');
+    }
   }
 
   console.log(`Saving themes data: ${themesData.length} themes`);
-  fs.writeFileSync('./data/themes.json', JSON.stringify(themesData));
+  fs.writeFileSync('./data/interfacelift.json', JSON.stringify(themesData));
 
   for (const theme of themesData) {
-    await sleep(1000);
-    console.log(`fetching ${theme.thumbnail}`);
-    const finalImg = await fetch(theme.thumbnail).then((res) => {
-      const imgPath = `./data/assets/${res.url.split('/').pop()}`;
-      const dest = fs.createWriteStream(imgPath);
-      res.body.pipe(dest);
-      return imgPath;
-    });
-    theme.thumbnail = path.resolve(__dirname, finalImg);
+    await sleep(200);
+    const imgPath = `./data/assets/${theme.thumbnail.split('/').pop()}`;
+    if (!fs.existsSync(imgPath)) {
+      console.log(`fetching ${theme.thumbnail}`);
+      await fetch(theme.thumbnail).then((res) => {
+        const dest = fs.createWriteStream(imgPath);
+        res.body.pipe(dest);
+        return imgPath;
+      });
+    } else {
+      console.log('already got', theme.thumbnail);
+    }
+
+    theme.thumbnail = path.resolve(__dirname, imgPath);
   }
+
+  console.log(`Saving themes data: ${themesData.length} themes`);
+  fs.writeFileSync('./data/interfacelift.json', JSON.stringify(themesData));
 }
 
 run();
---
+++ scrapers/interfacelift.js
@@ -0,0 +1,33 @@
+const cheerio = require('cheerio');
+
+exports.extractThemesFromBody = (htmlBody) => {
+  const $ = cheerio.load(htmlBody);
+
+  return $('.list_preview')
+    .map((i, el) => {
+      const $preview = $(el);
+
+      return {
+        thumbnail: $preview.find('img').attr('src'),
+        name: $preview
+          .next('.list_text')
+          .find('h1')
+          .text(),
+        author: $preview
+          .next('.list_text')
+          .find('h4')
+          .text()
+          .replace('by ', ''),
+      };
+    })
+    .get();
+};
+
+exports.getNextPage = (htmlBody) => {
+  const $ = cheerio.load(htmlBody);
+  const query = $('#pagenums > div:nth-child(2) > div:nth-child(2) > a[href]');
+
+  return query.length > 0 && query.attr('href');
+};
+
+exports.BASE_URL = '/web/20110126155426/https://interfacelift.com/themes-mac/';
