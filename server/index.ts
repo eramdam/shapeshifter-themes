ce2261e2784160e6f723306743cd956898bb621e
Damien Erambert <damien@erambert.me>
2022-01-24 22:29:22 -0800

    classic theme on even hours and modern one on odd hours

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,17 @@
 require("dotenv").config();
 import express from "express";
-import { random } from "lodash";
-import themes from "../data/merged.json";
+import { sample } from "lodash";
+import shapeshifterThemes from "../data/merged.json";
+import kaleidoscopeThemes from "../data/kaleidoscope.json";
 import { postThemeToMastodon, postThemeToTwitter } from "./post";
 
 const app = express();
 
 app.all(`/${process.env.BOT_ENDPOINT}`, async (req, res) => {
-  const theme = themes[random(0, themes.length - 1)];
+  const currentHourIsEven = new Date().getHours() % 2 === 0;
+  const theme = currentHourIsEven
+    ? sample(kaleidoscopeThemes) || kaleidoscopeThemes[0]
+    : sample(shapeshifterThemes) || shapeshifterThemes[0];
   await Promise.all([postThemeToMastodon(theme), postThemeToTwitter(theme)]);
   console.log(`Posted ${theme.name} - ${theme.author}`);
   res.sendStatus(200);
