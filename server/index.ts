4cd24f0b6046087874d88633717d838605d0350e
Damien Erambert <damien@erambert.me>
2022-02-14 20:50:18 +0100

    avoid posting the same themes twice

--- server/index.ts
+++ server/index.ts
@@ -4,26 +4,54 @@ import { sample } from "lodash";
 import shapeshifterThemes from "../data/merged.json";
 import kaleidoscopeThemes from "../data/kaleidoscope.json";
 import { postThemeToMastodon, postThemeToTwitter } from "./post";
+import fs from "fs/promises";
+import objectHash from "object-hash";
 
 const app = express();
 
-app.all(`/${process.env.BOT_ENDPOINT}`, async (req, res) => {
+async function pickTheme() {
+  const hashPath = "./data/tweeted-hashs.txt";
+  let tweetedHashs = (await fs.readFile(hashPath)).toString().split("\n");
   const currentHourIsEven = new Date().getHours() % 2 === 0;
-  const theme = currentHourIsEven
-    ? sample(kaleidoscopeThemes) || kaleidoscopeThemes[0]
-    : sample(shapeshifterThemes) || shapeshifterThemes[0];
+  const collection = currentHourIsEven
+    ? kaleidoscopeThemes
+    : shapeshifterThemes;
+
+  // If we tweeted everything, we reset the list
+  if (
+    tweetedHashs.length ===
+    kaleidoscopeThemes.length + shapeshifterThemes.length
+  ) {
+    tweetedHashs = [];
+  }
+
+  let pickedTheme = sample(collection) || collection[0];
+  let pickedHash = objectHash(pickedTheme);
+
+  while (tweetedHashs.find(h => h === pickedHash)) {
+    pickedTheme = sample(collection) || collection[0];
+    pickedHash = objectHash(pickedTheme);
+  }
+
+  tweetedHashs.push(pickedHash);
+
+  await fs.writeFile(hashPath, tweetedHashs.join("\n").trim());
+
+  return pickedTheme;
+}
+
+app.all(`/${process.env.BOT_ENDPOINT}`, async (req, res) => {
+  const theme = await pickTheme();
   await Promise.all([postThemeToMastodon(theme), postThemeToTwitter(theme)]);
   console.log(`Posted ${theme.name} - ${theme.author}`);
   res.sendStatus(200);
 });
 
 const listener = app.listen(process.env.PORT, () => {
-  if (process.env.NODE_ENV === "dev") {
-    console.log(
-      `Your bot is running on port http://localhost:${
-        // @ts-expect-error
-        listener.address().port
-      }/${process.env.BOT_ENDPOINT}`
-    );
-  }
+  console.log(
+    `Your bot is running on port http://localhost:${
+      // @ts-expect-error
+      listener.address().port
+    }/${process.env.BOT_ENDPOINT}`
+  );
 });
