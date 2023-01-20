1b8dc70a4c53d12040fb735171e01401cec601df
Damien Erambert <damien@erambert.me>
2023-01-20 12:38:11 -0800

    add download links for interfacelift

--- tools/scrape-interfacelift.ts
+++ tools/scrape-interfacelift.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
 import cheerio from "cheerio";
-import fs from "fs";
-import { compact, isString } from "lodash";
+import fs from "node:fs";
+import fetch from "node-fetch";
+import path from "node:path";
+import { compact, isString } from "lodash-es";
+import * as url from "url";
 
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+const sleep = (n: number) => new Promise(resolve => setTimeout(resolve, n));
 const fetchPage = (url: string) => axios.get(url).then(res => res.data);
 const BASE_URL = "/web/20110126155426/https://interfacelift.com/themes-mac/";
 
@@ -46,6 +52,7 @@ const BASE_URL = "/web/20110126155426/https://interfacelift.com/themes-mac/";
     const pageBody = await fetchPage(`https://web.archive.org${nextPage}`);
     themesData.push(...extractThemesFromBody(pageBody));
 
+    await sleep(500);
     nextPage = getNextPage(pageBody);
     console.log("Got themes", themesData.length);
 
@@ -53,6 +60,24 @@ const BASE_URL = "/web/20110126155426/https://interfacelift.com/themes-mac/";
       console.log("Next page", { nextPage });
     } else {
       console.log("No next page, all done!");
+    }
+  }
+
+  console.log(`Saving themes data: ${themesData.length} themes`);
+  fs.writeFileSync("./data/interfacelift.json", JSON.stringify(themesData));
+
+  for (const theme of themesData) {
+    await sleep(200);
+    const imgPath = `./assets/${theme.thumbnails[0].split("/").pop()}`;
+    if (!fs.existsSync(imgPath)) {
+      console.log(`fetching ${theme.thumbnails[0]}`);
+      await fetch(theme.thumbnails[0]).then(res => {
+        const dest = fs.createWriteStream(imgPath);
+        res.body?.pipe(dest);
+        return imgPath;
+      });
+    } else {
+      console.log("already got", theme.thumbnails);
     }
   }
 
